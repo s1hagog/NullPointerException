da72c9fd474fc1e55c8644862d6e97ef2d6da4f3
Patricio Garzon <nicox_g@hotmail.es>
2019-02-04 13:36:39 -0800

    Added Patricio Name

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -2,10 +2,11 @@ import java.io.Console;
 
 public class helloWorld {
 
+	public static String name = "Patricio Garzon";
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.print("Hello World");
+		//patricio garzon added
+		System.out.print("hello" + name);
 	}
 
 }
