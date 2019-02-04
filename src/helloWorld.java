78701b07b650e0a4219af736c1004b0ceb04ce69
Patricio Garzon <nicox_g@hotmail.es>
2019-02-04 13:34:15 -0800

    Hello World Application original commit

---
+++ src/helloWorld.java
@@ -0,0 +1,11 @@
+import java.io.Console;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("Hello World");
+	}
+
+}
